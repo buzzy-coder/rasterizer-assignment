63a327c710f5fe83af1ad0b1892eb47b3933b71c
buzzy-coder <tanisha985327@gmail.com>
2026-02-07 11:57:36 +0530

    Implement Bresenham line drawing algorithm

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,21 +2,43 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
-        ArrayList<Point> points = new ArrayList<>();
 
-        // add starting pixel
-        points.add(new Point(p1.x, p1.y));
+    ArrayList<Point> points = new ArrayList<>();
 
-        return points.toArray(new Point[0]);
-        int dx = Math.abs(p2.x - p1.x);
-        int dy = Math.abs(p2.y - p1.y);
+    int x1 = p1.x;
+    int y1 = p1.y;
+    int x2 = p2.x;
+    int y2 = p2.y;
 
-        int sx = (p1.x < p2.x) ? 1 : -1;
-        int sy = (p1.y < p2.y) ? 1 : -1;
+    int dx = Math.abs(x2 - x1);
+    int dy = Math.abs(y2 - y1);
 
-        int err = dx - dy;
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
 
+    int err = dx - dy;
 
+    while (true) {
+
+        points.add(new Point(x1, y1));
+
+        if (x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
     }
+
+    return points.toArray(new Point[0]);
+}
 
 }
